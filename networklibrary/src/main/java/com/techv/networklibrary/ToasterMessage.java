67fcb81921d9e7b04620cfc9fdedfc29e98ebb7e
Shobhan <shobhan.nalamasa@techvedika.com>
2019-07-26 15:10:34 +0530

    Initial Commit

---
+++ networklibrary/src/main/java/com/techv/networklibrary/ToasterMessage.java
@@ -0,0 +1,14 @@
+package com.techv.networklibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
